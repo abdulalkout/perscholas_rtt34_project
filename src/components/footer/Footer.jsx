ca592a593704f50b16ec77ade3cf4d8efd7ab598
drebyrd715 <drebyrd715@gmail.com>
2023-12-19 13:00:37 -0500

    footer changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,28 @@
 // Dre
-import React from "react";
+import React from 'react'
 
 function Footer() {
-  return <div>fooooooooooooooooooter</div>;
+  return (
+    <footer className="Footer">
+    <div>
+    <h2>PerScholas Instructors</h2>
+    <p>Teo Marcelo</p>
+    <p>Jordan Wofford</p>
+    <p>Dylan Comeau</p>
+    </div>
+    <div>
+    <h2>Devolpers</h2>
+    <p>Abdul Alkout</p>
+    <p>Jorge Robayo</p>
+    <p>Deandre Byrd</p>
+    <p>Mouloud Chabouni</p>
+    </div>
+    <div>
+    <h3>About</h3>
+    </div>
+    
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
