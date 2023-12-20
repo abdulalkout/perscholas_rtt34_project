be0b9d15fc3de8131f69c00a077959794cf7be97
abdulalkout <abdul.alkout@gmail.com>
2023-12-20 10:00:31 -0600

    Route installed

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 // importing contexts
 import DevelopersStoriesContextProvider from "./contexts/storiesContext/DevelopersStoriesContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DevelopersStoriesContextProvider>
-      <App />
-    </DevelopersStoriesContextProvider>
+    <Router>
+      <DevelopersStoriesContextProvider>
+        <App />
+      </DevelopersStoriesContextProvider>
+    </Router>
   </React.StrictMode>
 );
 
