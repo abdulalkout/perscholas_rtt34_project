65077e9ad3f28dff8202731978e94a2fc2629b28
drebyrd715 <drebyrd715@gmail.com>
2023-12-20 12:35:36 -0500

    about page

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return <div>
+     <h1 className="title-bottom-map">
+        Diverse Technological Talent On A National Scale
+      </h1>
+      <h3 className="titlebottom-paragraph">With 20 National Locations</h3>
+      <img className="perscholas-map" src="/perscholas-map.png" alt="Per Scholas Map" />
+      <div>
+        <h1>About Our Project!!</h1>
+        <p>We students here at RTT34 Cohort are collaboratively working on an innovative React.js project, 
+          showcasing our appreciation to Perscholas and dedication to mastering software engineering skills. 
+          Hailing from diverse backgrounds, each student brings a unique perspective to the table, enriching the project 
+          with varied experiences. As we progress through the course, our collective efforts reflect not only technical proficiency 
+          but also the resilience and adaptability cultivated in PerScholas dynamic learning environment. 
+          This React.js project stands as a testament to our growth over these last few months, blending individual strengths 
+          into a cohesive and impressive demonstration of our newfound expertise.</p>
+      </div>
+  </div>;
 }
 
 export default AboutPage;
