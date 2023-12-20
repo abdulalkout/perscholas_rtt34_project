e0b1dc2e0f2c6bb14406ad682a51b1597524e43d
mouloud <mouloud619@gmail.com>
2023-12-19 19:44:16 -0600

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 // Dre
-import React from "react";
+import React from 'react'
+import "./Footer.css"
 
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className="Footer">
+    <div>
+    <h2>PerScholas Instructors</h2>
+    <p>Teo Marcelo</p>
+    <p>Jordan Wofford</p>
+    <p>Dylan Comeau</p>
+    </div>
+    <div>
+    <h2>Devolpers</h2>
+    <p>Abdul Alkout</p>
+    <p>Jorge Robayo</p>
+    <p>Deandre Byrd</p>
+    <p>Mouloud Chabouni</p>
+    </div>
+    <div>
+    <h3>About</h3>
+    <p>Too</p>
+    </div>
+    
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
