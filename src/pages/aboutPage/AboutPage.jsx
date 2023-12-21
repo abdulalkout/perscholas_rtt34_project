46774d09aea9a6bb1f8ba4d4c590a24b7ddde4dd
jorge robayo <jorgerobmor@gmail.com>
2023-12-20 21:07:22 -0500

    testing

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -9,13 +9,15 @@ function AboutPage() {
       <img className="perscholas-map" src="/perscholas-map.png" alt="Per Scholas Map" />
       <div>
         <h1>About Our Project!!</h1>
-        <p>We students here at RTT34 Cohort are collaboratively working on an innovative React.js project, 
+        <p> We students here at RTT34 Cohort are collaboratively working on an innovative React.js project, 
           showcasing our appreciation to Perscholas and dedication to mastering software engineering skills. 
           Hailing from diverse backgrounds, each student brings a unique perspective to the table, enriching the project 
           with varied experiences. As we progress through the course, our collective efforts reflect not only technical proficiency 
           but also the resilience and adaptability cultivated in PerScholas dynamic learning environment. 
           This React.js project stands as a testament to our growth over these last few months, blending individual strengths 
-          into a cohesive and impressive demonstration of our newfound expertise.</p>
+          into a cohesive and impressive demonstration of our newfound expertise. </p>
+
+          {/* This is a test */}
       </div>
   </div>;
 }
