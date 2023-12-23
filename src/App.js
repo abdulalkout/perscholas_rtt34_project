60e20365041fb91483d00f2646726e69dba659fe
abdulalkout <abdul.alkout@gmail.com>
2023-12-22 20:56:14 -0600

    l

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // [importing components]
 import HomePage from "./pages/homePage/HomePage";
@@ -7,9 +8,21 @@ import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footer/Footer";
 import StudentsPage from "./pages/studentsStoriesPage/StudentsPage";
 import AboutPage from "./pages/aboutPage/AboutPage";
-
+import StudentApiList from "./components/studentsApi/StudentApiList";
+import DevelopersCooperation from "./pages/developersCooperation/DevelopersCooperation";
 
 function App() {
+  const [backend, setBackend] = useState();
+
+  const bringapi = async () => {
+    const response = await fetch("/developersCoop");
+    const data = await response.json();
+    setBackend(data);
+  };
+  useEffect(() => {
+    bringapi();
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
@@ -17,6 +30,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/students/:index" element={<StudentsPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/StudentApiList" element={<StudentApiList />} />
+        <Route path="/cooperation" element={<DevelopersCooperation />} />
       </Routes>
       <Footer />
     </div>
